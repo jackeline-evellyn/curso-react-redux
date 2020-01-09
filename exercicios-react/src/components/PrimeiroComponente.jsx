fda8bbb81cc2e954195b6939ba2e53546694b175
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2020-01-08 23:24:02 -0300

    Props e Multiplos Componentes em um arquivo

--- exercicios-react/src/components/PrimeiroComponente.jsx
+++ exercicios-react/src/components/PrimeiroComponente.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 
-
+let isLegal= false
 // Isso é uma função arrow function
-export default () => 
-// Aqui não se usa o return, pois implicitamente a função 
-// irá retornar um Componente
+export default (props) => 
+// é possível colocar o nome ue quiser como parametro, por padrão se coloca props
      <div>
-         <h1>Primeiro Componente</h1>
+         <h1>{props.valor}</h1>
+         <h1>{props.abcd}</h1>
+         <h1>Legal? {isLegal? 'Sim' : 'Não' }</h1>
+        <p>{Math.random()}</p> 
+    
      </div> 
+
+
+
+// ARROW FUNCTION
+// // Isso é uma função arrow function
+// export default () => 
+// // Aqui não se usa o return, pois implicitamente a função 
+// // irá retornar um Componente
+//      <div>
+//          <h1>Primeiro Componente</h1>
+//      </div> 
+// // Isso é uma função arrow function
+// export default () => 
+// // Aqui não se usa o return, pois implicitamente a função 
+// // irá retornar um Componente
+//      <div>
+//          <h1>Primeiro Componente</h1>
+//      </div> 
 
 
 // export default function primeiro(){
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PrimeiroComponente from './components/PrimeiroComponente'
+// import PrimeiroComponente from './components/PrimeiroComponente'
+
+// Como nao se usou o export default, se usa chaves
+import {CompA, CompB as B} from './components/DoisComponentes'
 
 const elemento = document.getElementById('root');
 
 ReactDOM.render(
     <div>
-        <PrimeiroComponente />
+
+        <CompA valor="Olá eu sou o A" />
+        <B valor="B na área" />
+        {/* <PrimeiroComponente valor="Bom dia" abcd={Math.pow(2,4)}/> */}
         {/* <h1>Ola react 3</h1>
         <ul>
             <li> 1) Pedro </li>
