8acb02839f1c68d779d84a686f80d02f765f4d13
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2020-01-08 22:47:55 -0300

    JSX, Componentes, Arrow function

---
+++ exercicios-react/src/components/PrimeiroComponente.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+// Isso é uma função arrow function
+export default () => 
+// Aqui não se usa o return, pois implicitamente a função 
+// irá retornar um Componente
+     <div>
+         <h1>Primeiro Componente</h1>
+     </div> 
+
+
+// export default function primeiro(){
+
+//     return <h1>Primeiro Componente</h1> 
+// }
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,2 +1,17 @@
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PrimeiroComponente from './components/PrimeiroComponente'
 
+const elemento = document.getElementById('root');
+
+ReactDOM.render(
+    <div>
+        <PrimeiroComponente />
+        {/* <h1>Ola react 3</h1>
+        <ul>
+            <li> 1) Pedro </li>
+            <li> 2) Maria </li>
+        </ul> */}
+    </div>
+, elemento
+)
