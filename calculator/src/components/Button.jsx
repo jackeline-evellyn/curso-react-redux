b798e7941a7e617aad32e9e4383331aa2053a8bb
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2020-01-11 12:00:40 -0300

    Estilização, uso de CSS Root com variaveis

---
+++ calculator/src/components/Button.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Button.css'
+
+export default props => {
+    let classes ='button'
+    // Caso possua a propriedade determinada será adicionado na classe 'button'
+    //E com isso realizando a estilização desejada
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+        <button 
+            onClick= {e => props.click && (e.target.label)}
+            className={classes}>
+                {props.label}
+            </button>
+    )
+}
+    
+   
+
+     
+
+// Outra forma de fazer:
+/* <button className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+    `}>
+</button> */
+
+/* A primeira classe (button) será aplicada independente
+    de qualquer regra, mas as outras passara por um filtro  */
+
+/* Ex: se a propriedade 'operation' for definida
+        a classe recebe a propriedade 'operation'
+    */
