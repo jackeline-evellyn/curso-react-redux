ff4e0c4e4da317000a78585c3567084a770fafdf
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2020-01-15 23:58:30 -0300

    Mudanças no frotend

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -4,12 +4,23 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props){
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+ 
+  handleAdd() {
+     //console.log('Add')
+     
+     console.log(this)
+   }
+   
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='cadastro'></PageHeader>
                 {/* Campos para adicionar */}
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} />
                 {/* Campo de Listagem */}
                 <TodoList />
             </div>
