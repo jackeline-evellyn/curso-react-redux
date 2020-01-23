50a707c5b14e3a751c2e43986cc71b978fcafd20
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2020-01-23 00:21:52 -0300

    Projeto Frontend sem Redux

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -1,5 +1,6 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
+import '../template/custom.css'
 //os arquivos pdem ser encontrados através de navegação pelas pastas
 
 import React from 'react'
--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -14,7 +14,9 @@ export default class Todo extends Component {
        
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
-        
+        this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
+
         //bind(this) garante que apotará para o mesmo objeto
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
@@ -24,9 +26,12 @@ export default class Todo extends Component {
     }
     //Pegar a lista mais atualizada
     refresh(description = ''){
+        //Caso haja algum valor na descrição, seu valor será setado
+        //Caso esteja vazio, retornará vazio
+        const search = description ? `&description__regex=/${description}/` : ''
         //sort vai ordenar de forma decrescente
-        axios.get(`${URL}?sort=-createAt`)
-        .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+        axios.get(`${URL}?sort=-createAt${search}`)
+        .then(resp => this.setState({...this.state, description, list: resp.data}))
     }
 
     handleChange(e){
@@ -39,29 +44,44 @@ export default class Todo extends Component {
    }
    handleRemove(todo){
         axios.delete(`${URL}/${todo._id}`)
-            .then(resp => this.refresh())
+            .then(resp => this.refresh(this.state.description))
    }
+   //marcar como feito
    handleMarkAsDone(todo){
        axios.put(`${URL}/${todo._id}`, {...todo, done: true})
-            .then(resp => this.refresh())
+            .then(resp => this.refresh(this.state.description))
    }
+   //marcar como pendente
    handleMarkAsPending(todo){
        axios.put(`${URL}/${todo._id}`,{...todo, done: false})
             .then(resp => this.refresh())
+   }
+   handleSearch(){
+    //    chama o metodo refresh como parametro a descrição
+        this.refresh(this.state.description)
+   } 
+   handleClear(){
+       this.refresh()
    }
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='cadastro'></PageHeader>
                 {/* Campos para adicionar */}
-                <TodoForm description={this.state.description}
-                handleAdd={this.handleAdd} 
-                handleChange={this.handleChange}/>
+                <TodoForm 
+                    description={this.state.description}
+                    handleAdd={this.handleAdd} 
+                    handleChange={this.handleChange}
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}
+                />
                 {/* Campo de Listagem */}
-                <TodoList list={this.state.list} 
+                <TodoList 
+                    list={this.state.list} 
                     handleMarkAsDone={this.handleMarkAsDone}
                     handleMarkAsPending={this.handleMarkAsPending}
-                    handleRemove={this.handleRemove}/>
+                    handleRemove={this.handleRemove}
+                />
             </div>
         )
     }
