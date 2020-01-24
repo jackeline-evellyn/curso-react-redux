3fe51323af96784fe039ad1d2e05c544b4b1d218
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2020-01-23 21:54:22 -0300

    Export, Import

--- react-16.4/exercicios/src/componentes/BomDia.jsx
+++ react-16.4/exercicios/src/componentes/BomDia.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 //usa-se props que é a convenção de propriedades
 //mas pode ser qualquer coisa ex: x
+
 export default props => 
-    <div>
-        <h1>Bom dia {props.nome}</h1>
-        <h2> Adeus e obrigado pelos peixes</h2>
-    </div>
+    [
+        //é necessário definir uma key
+        <h1 key='h1'>Bom dia {props.nome}</h1>,
+        <h2 key= 'h2'> Adeus e obrigado pelos peixes</h2>
+    ]
+
+// export default props => 
+//     <React.Fragment>
+//         <h1>Bom dia {props.nome}</h1>
+//         <h2> Adeus e obrigado pelos peixes</h2>
+//     </React.Fragment>
--- react-16.4/exercicios/src/index.js
+++ react-16.4/exercicios/src/index.js
@@ -2,5 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Primeiro from './componentes/Primeiro'
 import BomDia from './componentes/BomDia'
+
+// import BoaTarde, { BoaNoite} from './componentes/Multiplos'
 //const elemento = <h1>React 2 </h1>
-ReactDOM.render(<BomDia nome = "Guilherme"/>, document.getElementById('root'))
+
+import Multi from './componentes/Multiplos'
+
+ReactDOM.render(
+<div>
+    <Multi.BoaTarde nome="Ana"/>
+    <Multi.BoaNoite nome="Bia" />
+</div>
+, document.getElementById('root'))
