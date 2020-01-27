e6c8eb85ebcee2d0ed519075be891ecc5d22c869
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2020-01-27 09:28:13 -0300

    Componente de cadastro #03

--- crud/frontend/src/components/user/UserCrud.jsx
+++ crud/frontend/src/components/user/UserCrud.jsx
@@ -47,13 +47,60 @@ export default class UserCrud extends Component {
         return list
     }
 
+    updateField(event){
+        //operador spread (...), ou seja um clone
+        const user = {...this.state.user}
+        user[event.target.name] = event.target.value
+        this.setState({user})
+    }
+    renderForm(){
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control"
+                            name="name"
+                            value={this.state.user.name}
+                            onChange={e => this.updateField(e)}
+                            placeholder="Digite o nome ..." />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label> E-mail</label>
+                            <input type="text" className="form-control"
+                            name='email'
+                            value={this.state.user.email}
+                            onChange={e => this.updateField(e)}
+                            placeholder="Digite o email ..." />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 d-flex justify-content-end">
+                            <button className="btn btn-primary"
+                                onChange={e => this.save(e)}>
+                                Salvar
+                            </button>
+                            <button className="btn btn-secondary ml-2"
+                                onChange={e => this.clear(e)}>
+                                Cancelar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     // Antes do componente ser exibido será realizado essa função
    render(){
        return (
        
        <Main {...headerProps} >
-               Cadastro de Usuário
-            </Main>
+              {this.renderForm()}
+        </Main>
        )
    }
 }
